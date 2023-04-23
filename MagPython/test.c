301a5fd1d5d20502569b9ce35aa154c3d5ca2b5e
Chris Hofstaedtler <chris.hofstaedtler@deduktiva.com>
2023-04-23 21:00:26 +0200

    Add smoke test

---
+++ MagPython/test.c
@@ -0,0 +1,17 @@
+﻿#include <Python/python.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    printf("MagPython smoke test\n");
+    Py_NoSiteFlag++;
+    Py_Initialize();
+    printf("Compiler: %s\n", Py_GetCompiler());
+    int success = PyRun_SimpleString("import sys; sys.stdout.write('Hi from MagPython!\\nsys.path: %r\\n' % sys.path)");
+    if (success == 0)
+    {
+        return 0; // success
+    } else
+    {
+        return 1; // unknown error;
+    }
+}
