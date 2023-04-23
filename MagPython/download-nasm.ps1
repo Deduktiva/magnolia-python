638d57daa61ec90fce589710e5ddf0bb2a2aa3cf
Chris Hofstaedtler <chris.hofstaedtler@deduktiva.com>
2023-04-23 21:10:32 +0200

    download-nasm: make robust against preexisting files

--- MagPython/download-nasm.ps1
+++ MagPython/download-nasm.ps1
@@ -1,5 +1,8 @@
 # Download and unpack netwide assembler.
+$ErrorActionPreference = "Stop"
 $Version = "2.16.01"
+if (Test-Path nasm.zip) { del -force nasm.zip }
 Invoke-WebRequest -OutFile nasm.zip -Uri "https://www.nasm.us/pub/nasm/releasebuilds/$Version/win32/nasm-$Version-win32.zip"
+if (Test-Path nasm-$Version) { del -recurse -force nasm-$Version }
 Expand-Archive -Path nasm.zip -DestinationPath .
 ren nasm-$Version nasm
