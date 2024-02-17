Deduktiva/magnolia-python

commit 78a4fce788ab15ec91bddb0609c3b4307b60ddee

Python: treat some modules as statically linked
Original commit: ed53840f9137b0e206fb56bd6a753f52e6b24066

Python/PC/config.c
@@ -77,6 +77,12 @@ extern PyObject* PyInit__opcode(void);
 extern PyObject* PyInit__contextvars(void);
 extern PyObject* PyInit__tokenize(void);
 
+extern PyObject* PyInit__ctypes(void);
+extern PyObject* PyInit__ssl(void);
+extern PyObject* PyInit__hashlib(void);
+extern PyObject* PyInit__socket(void);
+extern PyObject* PyInit_unicodedata(void);
+
 /* tools/freeze/makeconfig.py marker for additional "extern" */
 /* -- ADDMODULE MARKER 1 -- */
 
@@ -175,6 +181,12 @@ struct _inittab _PyImport_Inittab[] = {
     {"_opcode", PyInit__opcode},
 
     {"_contextvars", PyInit__contextvars},
+
+    {"_ctypes", PyInit__ctypes},
+    {"_ssl", PyInit__ssl},
+    {"_hashlib", PyInit__hashlib},
+    {"_socket", PyInit__socket},
+    {"unicodedata", PyInit_unicodedata},
 
     /* Sentinel */
     {0, 0}

commit 668d40516294d0ab298db25bbba1a8d4bdb7cd55

Python: make selectmodule static
Fixes: 78a4fce788ab15ec91bddb0609c3b4307b60ddee

Python/PC/config.c
@@ -81,6 +81,7 @@ extern PyObject* PyInit__ctypes(void);
 extern PyObject* PyInit__ssl(void);
 extern PyObject* PyInit__hashlib(void);
 extern PyObject* PyInit__socket(void);
+extern PyObject* PyInit_select(void);
 extern PyObject* PyInit_unicodedata(void);
 
 /* tools/freeze/makeconfig.py marker for additional "extern" */
@@ -186,6 +187,7 @@ struct _inittab _PyImport_Inittab[] = {
     {"_ssl", PyInit__ssl},
     {"_hashlib", PyInit__hashlib},
     {"_socket", PyInit__socket},
+    {"select", PyInit_select},
     {"unicodedata", PyInit_unicodedata},
 
     /* Sentinel */